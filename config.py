368657f8d2a331cbd375064a3ff68468eb1606ce
sofyiq <150921026+sofyiq@users.noreply.github.com>
2024-04-01 18:19:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 
 
 # Token of Telegram bot
-TOKEN = ""
+TOKEN = "6604202754:AAEbN7c-02z989iNFERj5hx40UnO9qFEe_Q"
 
 
 # API key of TMDB (v3 auth)
-API_KEY= ""
+API_KEY= "8353e6bee3c75402cd26211fca1ff179"
